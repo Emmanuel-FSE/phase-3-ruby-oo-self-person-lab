a37597f0bc87f127ef6be51915bd11c0fabb932d
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-02-13 12:38:02 +0300

    Lab completed

--- lib/person.rb
+++ lib/person.rb
@@ -36,4 +36,52 @@ class Person
         "all about the benjamins"
     end
 
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        convo = case topic
+                when 'politics' then 'blah blah partisan blah lobbyist'
+                when 'weather' then 'blah blah sun blah rain'
+                else 'blah blah blah blah blah'
+                end
+    
+        adjust_happiness(person, topic)
+    
+        convo
+      end
+    
+      private
+    
+      def adjust_happiness(person, topic)
+        case topic
+        when 'politics'
+          self.happiness -= 2
+          person.happiness -= 2
+        when 'weather'
+          self.happiness += 1
+          person.happiness += 1
+        end
+    
+        self.happiness = 0 if self.happiness < 0
+        person.happiness = 0 if person.happiness < 0
+    
+        self.happiness = 10 if self.happiness > 10
+        person.happiness = 10 if person.happiness > 10
+      end
+
 end
